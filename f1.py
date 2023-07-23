4d727230e58fefc7775eabfa97bb0321aba30b1b
Muxlisa2007 <muxlisa200707@gmail.com>
2023-07-23 12:37:40 +0500

    1-masala

---
+++ f1.py
@@ -0,0 +1,7 @@
+a = float(input("Haqiyqiy sonni kiriting: "))
+b = int(input("Butun sonni iriting: "))
+PowerA3 = lambda a: a ** 3
+print("Haqiyqiy son darajasi:",PowerA3(a))
+
+PowerA3 = lambda b: b ** 3
+print("Butun son darajasi:",PowerA3(b))
