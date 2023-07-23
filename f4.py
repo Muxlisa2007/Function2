7274cfb7676d112082337f4b20da10fb9af01e75
Muxlisa2007 <muxlisa200707@gmail.com>
2023-07-23 12:45:12 +0500

    4-masala

---
+++ f4.py
@@ -0,0 +1,7 @@
+a = int(input("Teng tomonli uchburchakni kiriting: "))
+
+Triangle = lambda a: a + a + a
+print("Primetri:",Triangle(a))
+
+Triangle = lambda a: (a + a + a) * 3
+print("Yuzasi:",Triangle(a))
