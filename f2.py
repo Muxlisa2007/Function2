185b5cb44100938d49e202193f9b5a883f2b4134
Muxlisa2007 <muxlisa200707@gmail.com>
2023-07-23 12:41:24 +0500

    2-masala

---
+++ f2.py
@@ -0,0 +1,8 @@
+a = float(input("Kriting: "))
+
+PowerA234 = lambda a: a ** 2 
+print(PowerA234(a))
+PowerA234 = lambda a: a ** 3 
+print(PowerA234(a))
+PowerA234 = lambda a: a ** 4 
+print(PowerA234(a))
